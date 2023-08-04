8966b621a81cbda45a2ed1a8e9196e0ad87f4dee
MUEDSA <7676275+muedsa@users.noreply.github.com>
2023-08-04 09:51:47 +0800

    update: Singleton Foo

--- app/src/main/java/com/muedsa/bltv/AppModule.kt
+++ app/src/main/java/com/muedsa/bltv/AppModule.kt
@@ -4,10 +4,12 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal object AppModule {
     @Provides
+    @Singleton
     fun provideFoo() = Foo()
 }
--- app/src/main/java/com/muedsa/bltv/Foo.kt
+++ app/src/main/java/com/muedsa/bltv/Foo.kt
@@ -3,5 +3,18 @@ package com.muedsa.bltv
 import javax.inject.Inject
 
 class Foo @Inject constructor() {
-    val name = "foo"
+
+    val name: String
+
+    companion object {
+        @JvmStatic
+        var id: Int = 0
+    }
+
+    init {
+        synchronized(Foo) {
+            id += 1
+            name = "Foo $id"
+        }
+    }
 }
